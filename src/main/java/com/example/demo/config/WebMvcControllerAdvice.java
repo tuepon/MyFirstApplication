e4613c1c514c4fbe3c99219468bdde0be2611779
Ueno <tomonori_ueno@sezax.co.jp>
2022-10-23 10:57:30 +0900

    Update SampleController

--- src/main/java/com/example/demo/config/WebMvcControllerAdvice.java
+++ src/main/java/com/example/demo/config/WebMvcControllerAdvice.java
@@ -1,11 +1,8 @@
 package com.example.demo.config;
 
 import org.springframework.beans.propertyeditors.StringTrimmerEditor;
-import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.InitBinder;
 
 
